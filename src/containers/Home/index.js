e4c152883cc1dd12b89083581ba1356ceffe723f
Tursunpulat <Tursunpulat2002@gmail.com>
2019-07-19 16:03:36 -0700

    Init the bruhz

---
+++ src/containers/Home/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text } from './style'
+
+class Home extends Component {
+  render() {
+    return (
+      <Text>Welcome</Text>
+    )
+  }
+}
+
+export default Home
---
+++ src/containers/Home/style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+import { Flex } from "grid-styled"
+
+export const Text = styled(Flex)`
+    color: red;
+`
